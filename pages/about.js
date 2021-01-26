4bc52d0879be28f10e03319143748159772a9a1c
maracax <alaneraerts@live.be>
2021-01-26 22:42:28 +0100

    functional components

---
+++ pages/about.js
@@ -0,0 +1,27 @@
+
+// functional component - arrow function
+const About = () => {
+
+    const message = "Hello World"
+
+    return(
+    <h1> Hello About Page - { message } </h1>
+    )
+}
+
+// import * as React from "react";
+//
+// const About = () => {
+//
+//     const message = "Hello World"
+//     return React.createElement('h1',null ,'Generate Create Element')
+// }
+
+// unctional compoenent - normal function
+// function About() {
+//    return(
+//        <h1>Hello About Page</h1>
+//    )
+// }
+
+export default About;
