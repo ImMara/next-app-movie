611becbd217c7892a841650c731f9903aa129178
Mara <alaneraerts@live.be>
2021-01-28 19:27:07 +0100

    movie data and promises

--- components/movieList.js
+++ components/movieList.js
@@ -2,9 +2,9 @@ import * as React from "react";
 
 class MovieList extends React.Component {
 
-    shorten = (text,maxlength)  =>{
-        if(text && text.length > maxlength ){
-            return text.substr(0,maxlength) + '...';
+    shorten = (text, maxlength) => {
+        if (text && text.length > maxlength) {
+            return text.substr(0, maxlength) + '...';
         }
         return text;
     }
@@ -12,24 +12,24 @@ class MovieList extends React.Component {
     render() {
         const {movies} = this.props;
 
-        return(
+        return (
             <>
-                { movies.map( movie =>
+                {movies.map(movie =>
                     (
-                    <div className="col-lg-4 col-md-6 mb-4">
-                    <div className="card h-100">
-                    <a href="#"><img className="card-img-top" src={movie.image} alt=""/></a>
-                    <div className="card-body">
-                    <h4 className="card-title">
-                    <a href="#">{movie.name}</a>
-                    </h4>
-                    <p className="card-text">{this.shorten(movie.description,100)}</p>
-                    </div>
-                    <div className="card-footer">
-                    <small className="text-muted">{movie.rating}</small>
-                    </div>
-                    </div>
-                    </div>
+                        <div key={movie.id} className="col-lg-4 col-md-6 mb-4">
+                            <div className="card h-100">
+                                <a href="#"><img className="card-img-top" src={movie.image} alt=""/></a>
+                                <div className="card-body">
+                                    <h4 className="card-title">
+                                        <a href="#">{movie.name}</a>
+                                    </h4>
+                                    <p className="card-text">{this.shorten(movie.description, 100)}</p>
+                                </div>
+                                <div className="card-footer">
+                                    <small className="text-muted">{movie.rating}</small>
+                                </div>
+                            </div>
+                        </div>
                     )
                 )
                 }
@@ -37,4 +37,5 @@ class MovieList extends React.Component {
         )
     }
 }
+
 export default MovieList;
