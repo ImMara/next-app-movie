1f980fdc89ba142b7d8ed43bd66f78cf3ce3fdf8
maracax <alaneraerts@live.be>
2021-01-29 00:45:03 +0100

    movies in class component

--- pages/index.js
+++ pages/index.js
@@ -6,65 +6,73 @@ import MovieList from "../components/movieList";
 import Footer from "../components/footer";
 import {getMovies} from '../actions'
 import {useState,useEffect} from "react";
+import {Component} from "react/cjs/react.production.min";
 
-export default function Home() {
-    const [movies, setMovies] = useState([]);
-
-    useEffect( () => {
-
-        const fetchData = async () => {
-            const resMovies = await getMovies();
-            setMovies(resMovies);
+class Home extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            movies: [],
         }
-        fetchData();
-        // getMovies().then((movies) => {
-        //     setMovies(movies)
-        // })
+    }
+    // state = {
+    //     movies: []
+    // }
 
-    },[])
+    //called only once when component did mounted
+    componentDidMount() {
+        getMovies().then((movies) => {
+            this.setState({movies})
+        });
+
+    }
 
 
-    return (
-        <div>
-            <Head>
-                <title>Home</title>
-                <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-                      integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
-                      crossorigin="anonymous"/>
-                <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
-                        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
-                        crossOrigin="anonymous"></script>
-                <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"
-                        integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns"
-                        crossOrigin="anonymous"></script>
-            </Head>
-            <Navbar/>
-            <div className="home-page">
-            <div className="container">
-                <div className="row">
-                    <div className="col-lg-3">
-                        <SideMenu
-                            appName={"Movie DB"}
-                        />
-                    </div>
-                    <div className="col-lg-9">
-                        <Carousel/>
+    render() {
+        return (
+            <div>
+                <Head>
+                    <title>Home</title>
+                    <link rel="stylesheet"
+                          href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+                          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+                          crossorigin="anonymous"/>
+                    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+                            integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+                            crossOrigin="anonymous"></script>
+                    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"
+                            integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns"
+                            crossOrigin="anonymous"></script>
+                </Head>
+                <Navbar/>
+                <div className="home-page">
+                    <div className="container">
                         <div className="row">
-                            <MovieList
-                                movies={movies}
-                            />
+                            <div className="col-lg-3">
+                                <SideMenu
+                                    appName={"Movie DB"}
+                                />
+                            </div>
+                            <div className="col-lg-9">
+                                <Carousel/>
+                                <div className="row">
+                                    <MovieList
+                                        movies={this.state.movies}
+                                    />
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
+                <Footer/>
+                <style jsx>{`
+                  .home-page {
+                    padding-top: 80px;
+                  }
+                `}
+                </style>
             </div>
-        </div>
-            <Footer/>
-            <style jsx>{`
-            .home-page{
-            padding-top:80px;
-            }
-            `}
-            </style>
-        </div>
-    )
+        )
+    }
 }
+export default Home;
