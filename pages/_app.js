59497940db6c6940fee6319b61388639577e3968
maracax <alaneraerts@live.be>
2021-01-29 14:01:53 +0100

    app container props

--- pages/_app.js
+++ pages/_app.js
@@ -2,37 +2,46 @@ import '../styles/globals.css'
 import Navbar from "../components/navbar";
 import Head from "next/head";
 import Footer from "../components/footer";
+import App from "next/app";
 
-function MovieApp({ Component, pageProps }) {
-    return (
-        <>
-            <Head>
-                <title>Home</title>
-                <link rel="stylesheet"
-                      href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-                      integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
-                      crossorigin="anonymous"/>
-                <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
-                        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
-                        crossOrigin="anonymous"></script>
-                <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"
-                        integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns"
-                        crossOrigin="anonymous"></script>
-            </Head>
-            <Navbar/>
-            <div className="base-page">
-                <Component {...pageProps} />
-            </div>
-            <Footer/>
-            <style jsx>{`
+class MovieApp extends App {
+    static async getInitialProps(appContext) {
+        const appProps = await App. getInitialProps(appContext);
+        return {...appProps}
+    }
+
+    render() {
+        const {Component,pageProps} = this.props;
+        return (
+            <>
+                <Head>
+                    <title>Home</title>
+                    <link rel="stylesheet"
+                          href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+                          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+                          crossorigin="anonymous"/>
+                    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+                            integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+                            crossOrigin="anonymous"></script>
+                    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"
+                            integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns"
+                            crossOrigin="anonymous"></script>
+                </Head>
+                <Navbar/>
+                <div className="base-page">
+                    <Component {...pageProps} />
+                </div>
+                <Footer/>
+                <style jsx>{`
                   .base-page {
                     padding-top: 80px;
                   }
                 `}
-            </style>
-        </>
+                </style>
+            </>
 
-    )
+        )
+    }
 }
 
 export default MovieApp
