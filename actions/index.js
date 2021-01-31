c7f4fd9c68aad15c57c1028629c5ed4fd79c7a3e
maracax <alaneraerts@live.be>
2021-01-31 18:11:28 +0100

    Provide id of movie

--- actions/index.js
+++ actions/index.js
@@ -62,6 +62,7 @@ export const getMovies = () => {
 export const createMovie  = (movie) => {
 
     return new Promise((resolve, reject) => {
+        movie.id = Math.random().toString(36).substr(2,7)
         MOVIE_DATA.push(movie)
         setTimeout(() => {
             resolve(MOVIE_DATA)
@@ -69,6 +70,7 @@ export const createMovie  = (movie) => {
         }, 50)
     })
 }
+
 
 export const getMovieById = (id) => {
 
