188d27b0c7420fb0104dfc4d4c6609884005e658
maracax <alaneraerts@live.be>
2021-02-01 18:41:33 +0100

    server api

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const { createServer } = require('http')
 const { parse } = require('url')
+const bodyParser = require('body-parser')
 const next = require('next')
 const express = require('express')
 
@@ -9,8 +10,31 @@ const handle = app.getRequestHandler()
 
 app.prepare().then(( )=> {
     const server = express();
+    server.use(bodyParser.json())
+
+    server.get('/api/v1/movies',(req,res)=>{
+        return res.json({message:"hello world"})
+    })
+
+    // server.get('/faq',(req, res) =>{
+    //     res.send('<html><head></head><body><h1>hello world</h1></body></html>')
+    // })
+
     server.get('*', (req, res) => {
         return handle(req, res)
+    })
+    server.post('/api/v1/movies', (req, res) => {
+        const movie = req.body
+        console.log(JSON.stringify(movie))
+        return res.json({...movie,createdTime : 'today', author : 'testuser'})
+    })
+    server.patch('/api/v1/movies/:id', (req, res) => {
+        const { id } = req.params
+        return res.json({message:"updating post of id : "+id})
+    })
+    server.delete('/api/v1/movies/:id', (req, res) => {
+        const { id } = req.params
+        return res.json({message:"delete post of id : "+id})
     })
 
     const PORT = process.env.PORT || 3000;
